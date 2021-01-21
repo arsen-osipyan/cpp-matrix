711a3bab0a941299a94965de756fa07169fc87df
Arsen Osipyan <arsenosipan02work@gmail.com>
2021-01-21 15:59:00 +0300

    initial commit

---
+++ matrix.h
@@ -0,0 +1,24 @@
+﻿#ifndef MATRIX_H
+#define MATRIX_H
+
+
+#include <vector>
+
+
+class matrix
+{
+public:
+  matrix (size_t, size_t);
+  matrix (const std::vector<double>&);
+
+  double& operator () (size_t, size_t);
+  const double& operator () (size_t, size_t) const;
+
+private:
+  std::vector<double> data;
+  size_t wsize;
+  size_t hsize;
+};
+
+
+#endif // MATRIX_H
